c672acdc40015d35c52dd414c11f2cfe2e265ad6
gabrielareisvf <gabrielareisvf@gmail.com>
2025-07-14 00:10:03 -0300

    Fail Login

--- cypress/tests/e2e/login.spec.js
+++ cypress/tests/e2e/login.spec.js
@@ -1,3 +1,4 @@
+// Successful Login
 describe("Successful Login", () => {
   it("Should log in and show New Transaction page", () => {
     cy.visit("http://localhost:3000/signin");
@@ -5,5 +6,16 @@ describe("Successful Login", () => {
     cy.get("[name='password']").type("s3cret");
     cy.get("button[type='submit']").click();
     cy.contains("Public").should("be.visible");
+  });
+});
+
+// Fail Login
+describe("Fail Login", () => {
+  it("Should log in and show New Transaction page", () => {
+    cy.visit("http://localhost:3000/signin");
+    cy.get("[name='username']").type("GabiReis");
+    cy.get("[name='password']").type("12345");
+    cy.get("button[type='submit']").click();
+    cy.contains("Username or password is invalid").should("be.visible");
   });
 });
