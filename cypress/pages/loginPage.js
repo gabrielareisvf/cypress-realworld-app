927cb28f5a082f0227840a0c1e922a676ebb4488
gabrielareisvf <gabrielareisvf@gmail.com>
2025-07-14 21:34:01 -0300

    Creation of login pages

---
+++ cypress/pages/loginPage.js
@@ -0,0 +1,40 @@
+class LoginPage {
+  selectorsList() {
+    const selectors = {
+      usernameField: "[name='username']",
+      passwordField: "[name='password']",
+      loginButton: "[type='submit']",
+      wrongCredentialAlert: "[role='alert']",
+      getStarted: "[data-test='user-onboarding-dialog-title']",
+      mainLoginPage: "[data-test='nav-public-tab']",
+    };
+
+    return selectors;
+  }
+
+  acessLoginPage() {
+    cy.visit("http://localhost:3000/");
+  }
+
+  loginWithUser(username, password) {
+    cy.get(this.selectorsList().usernameField).type(username);
+    cy.get(this.selectorsList().passwordField).type(password);
+    cy.get(this.selectorsList().loginButton).click();
+  }
+
+  checkAccessInvalid() {
+    cy.get(this.selectorsList().wrongCredentialAlert)
+      .should("be.visible")
+      .and("contain", "Username or password is invalid");
+  }
+
+  checkFirstLoginPage() {
+    cy.get(this.selectorsList().getStarted).should("be.visible");
+  }
+
+  checkFirstLoginDonePage() {
+    cy.get(this.selectorsList().mainLoginPage).should("be.visible");
+  }
+}
+
+export default LoginPage;
--- cypress/tests/e2e/login.spec.js
+++ cypress/tests/e2e/login.spec.js
@@ -1,21 +1,18 @@
-// Successful Login
-describe("Successful Login", () => {
-  it("Should log in and show New Transaction page", () => {
-    cy.visit("http://localhost:3000/signin");
-    cy.get("[name='username']").type("Heath93");
-    cy.get("[name='password']").type("s3cret");
-    cy.get("button[type='submit']").click();
-    cy.contains("Public").should("be.visible");
-  });
-});
+import userData from "../../fixtures/user-data.json";
+import LoginPage from "../../pages/loginPage";
 
-// Fail Login
-describe("Fail Login", () => {
-  it("Should log in and show New Transaction page", () => {
-    cy.visit("http://localhost:3000/signin");
-    cy.get("[name='username']").type("GabiReis");
-    cy.get("[name='password']").type("12345");
-    cy.get("button[type='submit']").click();
-    cy.contains("Username or password is invalid").should("be.visible");
+const loginPage = new LoginPage();
+
+describe("Test Login", () => {
+  it("Login com sucesso", () => {
+    loginPage.acessLoginPage();
+    loginPage.loginWithUser(userData.userSuccess1.username, userData.userSuccess1.password);
+    loginPage.checkFirstLoginDonePage();
+  });
+
+  it("Login com falha", () => {
+    loginPage.acessLoginPage();
+    loginPage.loginWithUser(userData.userFail.username, userData.userFail.password);
+    loginPage.checkAccessInvalid();
   });
 });
